5b834f2bf5f399bef712966ff6eb87bf450de45d
Jisoo Park <jisoopark3@gmail.com>
2023-02-06 09:56:24 -0500

    services added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,25 @@
-import { Component,EventEmitter,Output } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] ;
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-    new Recipe('Another Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
-  ];
+  constructor(private recipeService: RecipeService){}
 
+  ngOnInit(): void {
+    
+    this.recipes = this.recipeService.getRecipes();
+  }
   onRecipeSelected(recipe: Recipe)
   {
-    this.recipeWasSelected.emit(recipe);
+    // this.recipeWasSelected.emit(recipe);
   }
-  constructor(){}
+  
 
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,18 @@
+import { EventEmitter } from "@angular/core";
+import { Recipe } from "./recipe.model";
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
+        new Recipe('Another Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+      ];
+
+    getRecipes()
+    {
+        return this.recipes.slice(); //new array as a copy
+    }
+
+    
+    
+}
