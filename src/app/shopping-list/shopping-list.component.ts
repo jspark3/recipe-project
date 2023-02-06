f9f2b0cccf7e5b29d6d9b366222c4ab2e82fe1ad
Jisoo Park <jisoopark3@gmail.com>
2023-02-06 10:11:00 -0500

    Dynamic changes to shoppinglist

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -14,6 +14,11 @@ export class ShoppingListComponent implements OnInit{
 
   ngOnInit(): void {
     this.ingredients = this.shoppinglistService.getIngredients();
+    this.shoppinglistService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,7 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredients.model";
 
 
 export class ShoppingListService{
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
@@ -15,5 +18,6 @@ export class ShoppingListService{
     addIngredient(ingredient: Ingredient)
     {
         this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
     }
 }
