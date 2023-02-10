b2983d1593e9eb8a1bd2c12950bb9deb4e886f74
Jisoo Park <jisoopark3@gmail.com>
2023-02-10 09:15:58 -0500

    Started to add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+//Array of js objest with routes
+const routes: Routes = [
+  //pathMatch => only redirect if the full path is empty
+  { path: '/', redirectTo: '/recipes', pathMatch: 'full' },
+  {path: '/recipes', component: RecipesComponent },
+  {path: '/shopping-list', component: ShoppingListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+//Provide and bound all routes we want to use
+export class AppRoutingModule { 
+
+
+}
